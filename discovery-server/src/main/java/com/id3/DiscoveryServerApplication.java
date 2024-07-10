4de36dfb8d26a2297414c645b835de00c65eabd5
RamazanAkdag <akdagramazan586@gmail.com>
2024-07-10 15:37:57 +0300

    discovery server initialized

--- discovery-server/src/main/java/com/id3/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/id3/DiscoveryServerApplication.java
@@ -2,16 +2,18 @@ package com.id3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-public class PersonnelInfoServiceApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(PersonnelInfoServiceApplication.class,args);
+        SpringApplication.run(DiscoveryServerApplication.class,args);
     }
 }
--- mail-service/src/main/java/com/id3/MailServiceApplication.java
+++ mail-service/src/main/java/com/id3/MailServiceApplication.java
@@ -3,10 +3,6 @@ package com.id3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
 public class MailServiceApplication
 {
